8f7d4c0109fe7949a13909e2651321704089e032
gazifahad <gazifahad3@gmail.com>
2022-04-18 20:08:02 +0600

    blog and about added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,6 +4,7 @@ const About = () => {
     return (
         <div>
             <h2 className='text-center text-secondary m-4'>This is Saiful Islam  </h2>
+            <p className='m-5'> I have recently finished BSC in CSE,And then I was strugling about choosing a specific goal , Then I found this course And got to know about the reputation of jhankar mahbub sir as an instructor,And I has some sort of interest in web developing even I had some interest in MERN before,But After enrolling in the course I found myself very much interested in react and everything,credit goes to the great jhankar mahabub sir and his team, And now my motto is finishing this course timely then getting a chance in SCIC ,doing some really good presentable projects. I feel myself a bit lazy these days, BUt I will start to change things in sha ALLAH.</p>
         </div>
     );
 };
